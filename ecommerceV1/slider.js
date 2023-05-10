0eefc1b598577d27bbd9a7d5837ea94be3e19775
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-05-10 15:12:33 -0400

    added comments to code

--- ecommerceV1/slider.js
+++ ecommerceV1/slider.js
@@ -1,15 +1,22 @@
 let slideIndex = 1;
+// initializes the slideIndex to 1 
 showSlides(slideIndex);
+// first slide should be displayed when page loads. 
+
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
+// controlled by next and previous buttons te determine to move forward or backwards. calls the slow slides function
 
 // Thumbnail image controls
+// controls which slide to display 
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
+
 
 function showSlides(n) {
   let i;
